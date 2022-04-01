3a789c2845fb0ef6073c1359e685adfc53dac057
Leon Acosta <leon@dandelionlabs.io>
2022-04-01 22:18:19 +0700

    Modified cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.options('*', cors())
 
 app.get("/", (_req, res) => {
   res.send("Token Linear Vesting Grant list!");
