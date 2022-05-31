d7ebcbdcfb3fbb10ee05abd15a8aaf23cd1b504c
Leon Acosta <leon@dandelionlabs.io>
2022-05-31 15:40:29 +0700

    sda

--- database/sequelize.js
+++ database/sequelize.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(
         host: process.env.DATABASE_HOST,
         port: process.env.DATABASE_PORT,
         dialect: process.env.DATABASE_DIALECT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
         database: process.env.DATABASE_NAME,
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
